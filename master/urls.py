466f28479de081250081c5f295b8c3f427d36dd3
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-03-08 09:37:36 +0530

    details api

--- master/urls.py
+++ master/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import taskListAPI
+from .views import taskListAPI,taskDetailAPI
 urlpatterns = [
     path('', taskListAPI, name='taskListAPI'),
+    path('task/<int:task_id>', taskDetailAPI, name='taskDetailAPI')
 ]
--- master/views.py
+++ master/views.py
@@ -20,7 +20,37 @@ def taskListAPI(request):
             return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 def taskDetailAPI(request, task_id):
-    pass
+    try:
+        querySet = TaksModel.objects.get(id=task_id)
+    except TaksModel.DoesNotExist:
+        return Response({
+            'message':'Data not found'
+        }, status=status.HTTP_404_NOT_FOUND)
+    else:
+        if request.method == 'GET':
+            serializers= TaksSerializers(querySet)
+            return Response(serializers.data, status=status.HTTP_200_OK)
+        
+        
+        if request.method == 'PUT':
+            serializers = TaksSerializers(querySet, data=request.data)
+            if serializers.is_valid():
+                serializers.save()
+                return Response(serializers.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+        if request.method == 'PATCH':
+            serializers = TaksSerializers(querySet, data=request.data, partial=True)
+            if serializers.is_valid():
+                serializers.save()
+                return Response(serializers.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+        if request.method == 'DELETE':
+            querySet.delete()
+            return Response({'message':"Data deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
